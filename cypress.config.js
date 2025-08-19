98045a0e5dd4a502aed313f54998b1726ded7080
ornops <joshuaornopia@gmail.com>
2025-08-19 22:58:27 +0800

    Added First test

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  video: false,
   e2e: {
+    baseUrl: 'http://localhost:4200/pages',
+    excludeSpecPattern: ['**/1-getting-started', '**/2-advanced-examples'],
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/dummy-test.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress"/>
+
+describe('Login Test Suite', () => {
+
+	// beforeEach(() => {
+	// 	cy.visit('/');
+	// 	cy.get('#user-name').type('standard_user');
+	// 	cy.get('#password').type('secret_sauce');
+	// 	cy.get('#login-button').click();
+	// });
+
+	// it('Check Products Page as default page', () => {
+	// 	cy.get('.app_logo').should('contain', 'Swag Labs');
+	// });
+
+	// it('Check About page', () => {
+	// 	cy.get('#react-burger-menu-btn').click();
+	// 	cy.get('#about_sidebar_link').click();
+	// 	cy.url().should('include', 'https://saucelabs.com/');
+	// });
+
+});
